54158aefd30f33945fa7b48ce59f29c6dbeb1941
Oleksandra <150050230+olexandratabakova@users.noreply.github.com>
2025-09-07 11:52:38 +0300

    Delete secrets.py

--- secrets.py
+++
@@ -1,5 +0,0 @@
-API_KEY_newsapi = "187606bb82c449b58f907710dd54b0c7" #https://newsapi.org/
-
-API_KEY_newsdata = "pub_a3f4ddc1f57c421ab7f9d812d51e4cf2"
-
-API_KEY_worldnewsapi = "187606bb82c449b58f907710dd54b0c7"
